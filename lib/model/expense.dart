badbee1dfe38e85b0b7b1a942af21008474ae8f3
Wendy Private <lienyun.chang@gmail.com>
2024-09-25 15:52:04 +0800

    105. date formatter: intl

--- lib/model/expense.dart
+++ lib/model/expense.dart
@@ -1,9 +1,20 @@
 import 'package:uuid/uuid.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
 
 const uuid = Uuid();
 
 // enum適合拿來當類別
 enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
 
 class Expense {
   Expense({
@@ -18,4 +29,8 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }

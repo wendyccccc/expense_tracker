57860f9ee50beb075e551e2f53edc3cfcaabf3e6
Wendy Private <lienyun.chang@gmail.com>
2024-09-25 15:39:54 +0800

    104.

--- lib/widgets/expenses_list/expenses_item.dart
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -8,12 +8,34 @@ class ExpensesItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 16,
-        ),
-        child: Text(expense.title),
-      ),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 16,
+          ),
+          child: Column(
+            children: [
+              Text(expense.title),
+              const SizedBox(
+                height: 4,
+              ),
+              Row(
+                // toStringAsFixed: 轉為字串並取小數點後兩位
+                children: [
+                  Text('\$${expense.amount.toStringAsFixed(2)}'),
+                  const Spacer(),
+                  Row(
+                    children: [
+                      const Icon(Icons.alarm),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Text(expense.date.toString()),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          )),
     );
   }
 }
